906a4b9a59af8738b42f4a606a6aab2c7d3733a1
Voidberry <98706885+V0idberry@users.noreply.github.com>
2023-05-15 23:27:01 +0200

    Changed the playRound fundtion so that the counter works. Added event listeners which work and display current score on console log.

--- script.js
+++ script.js
@@ -17,35 +17,63 @@ function gameRound(playerSelection, computerSelection) {
     if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
         return('"' + playerSelection + '"' + " isn't a valid input. Please use rock, paper or scissors");
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        playerScore =+ 1;
+        playerScore += 1;
         return ('You Win! Rock beats Scissors!');
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        playerScore =+ 1;
+        playerScore *= 1;
         return ('You Win! Paper beats Rock!');
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        playerScore =+ 1;
+        playerScore += 1;
         return ('You Win! Scissors beat Paper!');
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        computerScore =+ 1;
+        computerScore += 1;
         return ('You Lose! Paper beats Rock!');
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        computerScore =+ 1;
+        computerScore += 1;
         return ('You Lose! Scissors beat Paper!');
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        computerScore =+ 1;
+        computerScore += 1;
         return ('You Lose! Rock beats Scissors!');
     } else {
         return ("It's a Draw!")
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Please type your choice: rock, paper or scissors").toLowerCase();
-        const computerSelection = getComputerChoice();
-        console.log(gameRound(playerSelection, computerSelection));
-    }
-    console.log("Player score: " + playerScore + ". Computer score: " + computerScore + ".");
-}
+const btnRock = document.getElementById('btnRock');
+const btnPaper = document.getElementById('btnPaper');
+const btnScissors = document.getElementById('btnScissors');
 
-game();
+btnRock.addEventListener('click', (e) => {
+    playerSelection = e.target.innerText.toLowerCase();
+    computerSelection = getComputerChoice();
+    console.log(gameRound(playerSelection, computerSelection));
+    console.log(`Player score: ${playerScore}`);
+    console.log(`Computer score: ${computerScore}`);
+});
+
+btnPaper.addEventListener('click', (e) => {
+    playerSelection = e.target.innerText.toLowerCase();
+    computerSelection = getComputerChoice();
+    console.log(gameRound(playerSelection, computerSelection));
+    console.log(`Player score: ${playerScore}`);
+    console.log(`Computer score: ${computerScore}`);
+});
+
+btnScissors.addEventListener('click', (e) => {
+    playerSelection = e.target.innerText.toLowerCase();
+    computerSelection = getComputerChoice();
+    console.log(gameRound(playerSelection, computerSelection));
+    console.log(`Player score: ${playerScore}`);
+    console.log(`Computer score: ${computerScore}`);
+});
+
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         const playerSelection = prompt("Please type your choice: rock, paper or scissors").toLowerCase();
+//         const computerSelection = getComputerChoice();
+//         console.log(gameRound(playerSelection, computerSelection));
+//     }
+//     console.log("Player score: " + playerScore + ". Computer score: " + computerScore + ".");
+// }
+
+// game();
